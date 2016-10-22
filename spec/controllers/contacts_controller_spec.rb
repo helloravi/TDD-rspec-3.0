6674760b5b3adddf3b8ad7f986ac5a2cd497537d
Ravikanth Andhavarapu <raviandkanth@gmail.com>
2016-10-22 21:14:04 +0530

    Add specs for create method in contacts controller

--- spec/controllers/contacts_controller_spec.rb
+++ spec/controllers/contacts_controller_spec.rb
@@ -47,23 +47,58 @@ describe ContactsController do
   end
 
   describe "GET #new" do
-    it "assigns the requested contact to @contact"
-    it "renders the :new template"
+    it "assigns the requested contact to @contact" do
+      get :new
+      expect(assigns(:contact)).to be_a_new(Contact)
+    end
+
+    it "renders the :new template" do
+      get :new
+      expect(response).to render_template :new
+    end
   end
 
   describe "GET #edit" do
-    it "assigns the requested contact to @contact"
-    it "renders the :edit template"
+    it "assigns the requested contact to @contact" do
+      contact = create(:contact)
+      get :edit, id: contact
+      expect(assigns(:contact)).to eq contact
+    end
+
+    it "renders the :edit template" do
+      contact = create(:contact)
+      get :edit, id: contact
+      expect(response).to render_template :edit
+    end
   end
 
   describe "POST #create" do
+
+      before :each do
+        @phones = [
+          attributes_for(:phone),
+          attributes_for(:phone),
+          attributes_for(:phone)
+        ]
+      end
     context "with valid attributes" do
-      it "saves the new contact in the database"
-      it "redirects the user to contacts#show"
+      it "saves the new contact in the database" do
+        expect {post :create, contact: attributes_for(:contact, phones_attributes: @phones)}.to change(Contact, :count).by(1)
+      end
+
+      it "redirects the user to contacts#show" do
+        post :create, contact: attributes_for(:contact, phones_attributes: @phones)
+        expect(response).to redirect_to contact_path(assigns[:contact])
+      end
     end
     context "with invalid attributes" do
-      it "does not save the contact to the database"
-      it "re-renders the :new template"
+      it "does not save the contact to the database" do
+        expect { post :create, contact: attributes_for(:invalid_contact, phone_attributes: @phone) }.to_not change(Contact, :count)
+      end
+      it "re-renders the :new template" do
+        post :create, contact: attributes_for(:invalid_contact, phones_attributes: @phones)
+        expect(response).to render_template :new
+      end
     end
   end
 
